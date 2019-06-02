ea76ec0af46cdc3bd1afadbc0289c17108bf3090
Ben Liu <ben196888@gmail.com>
2019-06-03 08:53:05 +1000

    feat(frontend): parse dropped files and save them in the new bookings state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
+import csvtojson from 'csvtojson';
 import './App.css';
 
 const apiUrl = 'http://localhost:3001'
@@ -16,8 +17,40 @@ class App extends Component {
       })
   }
 
-  onDrop(files) {
-    console.log(files);
+  /**
+   * handle the dropped files
+   * assume all the files are csv format
+   * @param {*} files
+   * @memberof App
+   */
+  onDrop = (files) => {
+    const reader = new FileReader();
+    reader.onabort = () => console.log('file reading was aborted')
+    reader.onerror = () => console.log('file reading has failed')
+    reader.onload = () => csvtojson()
+      .fromString(reader.result)
+      .then(jsonObj => {
+        const newBookings = jsonObj.map(booking => ({
+          userId: booking.userId,
+          time: (new Date(booking.time)).getTime(),
+          duration: Number(booking.duration) * 60 * 1000 // min to ms
+        }));
+
+        newBookings.forEach((nB, i) => {
+          // non-overlap condition: A[start, end] <= B[start, end] or B[start, end] <= A[start, end]
+          // additional condition between new bookings: different booking (index)
+          // TODO: optimise the overlap evaluation with sort
+          const isNotOverlap = this.state.bookings.every(b => (nB.time + nB.duration) <= b.time || (b.time + b.duration) <= (nB.time))
+            && newBookings.every((b, j) => i === j || (nB.time + nB.duration) <= b.time || (b.time + b.duration) <= (nB.time));
+          if (!isNotOverlap) {
+            nB.overlap = true;
+          }
+        });
+
+        this.setState({ newBookings });
+      });
+
+    files.forEach(file => reader.readAsText(file));
   }
 
   render() {
